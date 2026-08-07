206acb791189a0af9876de5c28f9907ec8a809ed
yeang976-art <yeang976@gmail.com>
2026-08-07 14:34:21 +0900

    푸시좀 하자

--- Profile_Project/src/main/java/com/example/profile_project/entity/PersonalInfo.java
+++ Profile_Project/src/main/java/com/example/profile_project/entity/PersonalInfo.java
@@ -14,7 +14,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "profiles")
+@Table
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PersonalInfo extends BaseEntity {
